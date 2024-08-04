1afb6e2c0004e62e19efef998053b343163a6d4c
Baybars Orhun Şahin <borhunsahin@outlook.com>
2024-08-04 13:19:22 +0300

    Character Added

---
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    PlayerController playerController;
+    public Animator animator;
+    void Start()
+    {
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {      
+        animator.SetFloat("inputX",playerController.input.x * playerController.currentSpeedMultiplier);
+        animator.SetFloat("inputY", playerController.input.y * playerController.currentSpeedMultiplier);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController characterController;
+    private PlayerAnimationController playerAnimController;
+
+    public Vector2 input;
+    private Vector3 direction;
+
+    [SerializeField] public Player player;
+    [SerializeField] public Gravity gravity;
+
+    private Camera playerCam;
+
+    [HideInInspector] public float currentSpeedMultiplier;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        playerAnimController = GameObject.Find("Player").GetComponentInChildren<PlayerAnimationController>();
+        playerCam = Camera.main;
+
+        currentSpeedMultiplier = 1;
+    }
+    void Update()
+    {
+
+        FreeMode();
+        GravityApply();
+        Movement();
+
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<Vector2>();
+        direction = new Vector3(input.x, 0, input.y); 
+    }
+    private void Movement()
+    {
+        characterController.Move((new Vector3(direction.x * currentSpeedMultiplier, direction.y, direction.z * currentSpeedMultiplier) * player.speed  * Time.deltaTime));
+    }
+    public void Sprint(InputAction.CallbackContext context)
+    {
+        if (context.started && player.isFreeMode)
+            currentSpeedMultiplier = player.speedMultiplier;
+        //else if(context.started && player.isActionMode && !player.isCrouch)
+            //currentSpeedMultiplier = player.speedMultiplier;
+        else if (context.canceled)
+            currentSpeedMultiplier = 1;
+    }
+    public void Interact(InputAction.CallbackContext context)
+    {
+        if (!context.started) return;
+
+        Debug.Log("Interact F");
+    }
+    private void FreeMode()
+    {
+        if (input.sqrMagnitude > 0.05)
+        {
+            direction = Quaternion.Euler(0.0f, playerCam.transform.eulerAngles.y, 0.0f) * new Vector3(input.x, 0.0f, input.y);
+            var targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 500f * Time.deltaTime);
+        }
+    }
+    private void GravityApply()
+    {
+        if (characterController.isGrounded && gravity.velocity < -2)
+        {
+            gravity.velocity = -1f;
+        }
+        else
+        {
+            gravity.velocity += gravity.gravity * gravity.gravityMultipler * Time.deltaTime;
+            direction.y = gravity.velocity;
+        }
+    }
+}
+[Serializable]
+public struct Player
+{
+    public float speed;
+    public float speedMultiplier;
+
+    public bool isFreeMode;
+}
+[Serializable]
+public struct Gravity
+{
+    public float gravity;
+    public float gravityMultipler;
+    [HideInInspector] public float velocity;
+}
